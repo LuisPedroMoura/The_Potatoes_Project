e8223028c31b1ff69bd0e1190541be100ce674ef
pedrovt <pedro.teix@ua.pt>
2018-05-14 21:04:03 +0100

    Added Basic Folder Struture

---
+++ src/potatoesGrammar/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <b>package-info</b><p>
+ * 
+ * @author Inês Justo (84804), Luis Pedro Moura (83808), Maria João Lavoura (84681), Pedro Teixeira (84715)
+ * @version May-June 2018
+ */
+package potatoesGrammar;
