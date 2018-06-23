b13199f8cbe4c06ffec67f935481095ab140b949
pedrovt <pedro.teix@ua.pt>
2018-06-23 21:46:45 +0100

    Added / FIxed Tests. Add UI to test Graphs. Eclipse Project fixes

---
+++ src/README.txt
@@ -0,0 +1,5 @@
+To Install Needed Dependencies, please add the JUNG jars to the classpath executing the following command:
+sudo cp utils/jung2-2_0_1/*.jar /usr/java/packages/lib/ext/
+
+
+Without this command, antlr4-build won't work, even if Eclipse doesn't give any error.
--- src/tests/typesGrammar/testTypesFileInfo.java
+++ src/tests/typesGrammar/testTypesFileInfo.java
@@ -3,18 +3,21 @@ package tests.typesGrammar;
 import typesGrammar.TypesFileInfo;
 
 /**
- * <b>testTypesFileInfo</b><p>
+ * <b>TestTypesFileInfo</b><p>
  * 
  * @author Inês Justo (84804), Luis Pedro Moura (83808), Maria João Lavoura (84681), Pedro Teixeira (84715)
  * @version May-June 2018
  */
-public class testTypesFileInfo {
+public class TestTypesFileInfo {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		TypesFileInfo file = new TypesFileInfo("C:\\Users\\Pedro\\Desktop\\LFA_Project\\LFA_Project\\src\\tests\\typesGrammar\\TypesExample3.txt");
+
+		//".\\src\\tests\\typesGrammar\\TypesExample1.txt"
+		System.out.println("Testing File " + args[0]);
+		TypesFileInfo file = new TypesFileInfo(args[0]);
 		System.out.println(file);
 
 	}
