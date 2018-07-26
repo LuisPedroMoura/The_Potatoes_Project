f179e9e042760e38810681a9df568797a0b34077
Luis Moura <luispedromoura@ua.pt>
2018-07-26 20:48:35 +0100

    GraphInfo
    Added more methods that return new Graphs with all information of shortest paths (basically direct connections between vertices with all total cost of shortest path)

--- src/utils/GraphInfo.java
+++ src/utils/GraphInfo.java
@@ -24,11 +24,17 @@ public class GraphInfo<V,E> extends Graph<V,E>{
 	Graph<V,E> graph;
 	List<ArrayList<V>> allShortestPaths;
 	List<ArrayList<V>> allStraightFowardPaths;
+	Graph<V,Double> shortestPathsGraph;
+	Graph<V,Double> straightfowardPathsGraph;
+	Graph<V,Double> straightfowardPathsCostsGraph;
 	
 	GraphInfo(Graph<V,E> graph) {
 		this.graph = graph;
 		allShortestPaths = getAllShortestPaths(graph);
 		allStraightFowardPaths = getAllStraightfowardPaths(graph);
+		createShortestPathsGraph();
+		createStraightfowardPathsGraph();
+		createStraightfowardPathsCostsGraph();
 	}
 	
 	/**
@@ -60,7 +66,44 @@ public class GraphInfo<V,E> extends Graph<V,E>{
 		}
 		return allPaths;
 	}
-	 
+	
+	
+	private void createShortestPathsGraph(){
+		double pathCost = 0.0;
+		for (List<V> list : allShortestPaths) {
+			for (int i = 0; i < list.size()-1; i++) {
+				pathCost *= graph.getEdgeCost(graph.getEdge(list.get(i), list.get(i+1)));
+			}
+			shortestPathsGraph.addEdge(pathCost, list.get(0), list.get(list.size()-1));
+			shortestPathsGraph.addEdge(pathCost, list.get(list.size()-1), list.get(0));
+			pathCost = 0.0;
+		}
+	}
+	
+	private void createStraightfowardPathsGraph(){
+		double pathCost = 0.0;
+		for (List<V> list : allStraightFowardPaths) {
+			for (int i = 0; i < list.size()-1; i++) {
+				pathCost += 1;
+			}
+			straightfowardPathsGraph.addEdge(pathCost, list.get(0), list.get(list.size()-1));
+			straightfowardPathsGraph.addEdge(pathCost, list.get(list.size()-1), list.get(0));
+			pathCost = 0.0;
+		}
+	}
+	
+	
+	private void createStraightfowardPathsCostsGraph(){
+		double pathCost = 0.0;
+		for (List<V> list : allStraightFowardPaths) {
+			for (int i = 0; i < list.size()-1; i++) {
+				pathCost *= graph.getEdgeCost(graph.getEdge(list.get(i), list.get(i+1)));
+			}
+			straightfowardPathsCostsGraph.addEdge(pathCost, list.get(0), list.get(list.size()-1));
+			straightfowardPathsCostsGraph.addEdge(pathCost, list.get(list.size()-1), list.get(0));
+			pathCost = 0.0;
+		}
+	}
 	
 	
 }
