9101a8fb4f56a5b0dfa190c4bfd6fa7fb391ec65
Luis Moura <luisppmoura@gmail.com>
2018-08-08 18:51:39 +0100

    deleting package infos

--- src/tests/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <b>tests</b><p>
- * This package contains source code to test both the Potatoes and Types Languages.
- * @author Ines Justo (84804), Luis Pedro Moura (83808), Maria Joao Lavoura (84681), Pedro Teixeira (84715)
- * @version May-June 2018
- */
-package tests;
